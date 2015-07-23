9f1c436d91ca1c55ed3f36fa12e3db9203298ee5
kvv20 <kvv20@users.noreply.github.com>
2015-07-23 11:15:32 -0400

    working communication with Android

--- src/main.c
+++ src/main.c
@@ -1,60 +1,122 @@
-/*
- * main.c
- * Creates a Window, TextLayer and shows the number of seconds, minutes 
- * and hours since launch.
- */
-
 #include <pebble.h>
 
+#define KEY_BUTTON    0
+#define KEY_VIBRATE   1
+
+#define BUTTON_UP     0
+#define BUTTON_SELECT 1
+#define BUTTON_DOWN   2
+
 static Window *s_main_window;
-static TextLayer *s_uptime_layer;
+static TextLayer *s_text_layer;
 
-static int s_uptime = 0;
+/******************************* AppMessage ***********************************/
 
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  // Use a long-lived buffer
-  static char s_uptime_buffer[32];
+static void send(int key, int message) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
 
-  // Get time since launch
-  int seconds = s_uptime % 60;
-  int minutes = (s_uptime % 3600) / 60;
-  int hours = s_uptime / 3600;
+  dict_write_int(iter, key, &message, sizeof(int), true);
 
-  // Update the TextLayer
-  snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "Uptime: %dh %dm %ds", hours, minutes, seconds);
-  text_layer_set_text(s_uptime_layer, s_uptime_buffer);
-
-  // Increment s_uptime
-  s_uptime++;
+  app_message_outbox_send();
 }
 
-static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
+static void inbox_received_handler(DictionaryIterator *iterator, void *context) {
+  // Get the first pair
+  Tuple *t = dict_read_first(iterator);
 
-  // Create output TextLayer
-  s_uptime_layer = text_layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
-  text_layer_set_text_alignment(s_uptime_layer, GTextAlignmentCenter);
-  text_layer_set_text(s_uptime_layer, "Uptime: 0h 0m 0s");
-  layer_add_child(window_layer, text_layer_get_layer(s_uptime_layer));
+  // Process all pairs present
+  while(t != NULL) {
+    // Process this pair's key
+    switch(t->key) {
+      case KEY_VIBRATE:
+        // Trigger vibration
+        text_layer_set_text(s_text_layer, "Vibrate!");
+        vibes_short_pulse();
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_INFO, "Unknown key: %d", (int)t->key);
+        break;
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }
+}
+
+static void inbox_dropped_handler(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_handler(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_handler(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+/********************************* Buttons ************************************/
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_text_layer, "Select");
+
+  send(KEY_BUTTON, BUTTON_SELECT);
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_text_layer, "Up");
+
+  send(KEY_BUTTON, BUTTON_UP);
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_text_layer, "Down");
+
+  send(KEY_BUTTON, BUTTON_DOWN);
+}
+
+static void click_config_provider(void *context) {
+  // Assign button handlers
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+/******************************* main_window **********************************/
+
+static void main_window_load(Window *window) {
+  // Create main TextLayer
+  s_text_layer = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text(s_text_layer, "Open Android app and press any button.");
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy output TextLayer
-  text_layer_destroy(s_uptime_layer);
+  // Destroy main TextLayer
+  text_layer_destroy(s_text_layer);
 }
 
 static void init(void) {
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_handler);
+  app_message_register_inbox_dropped(inbox_dropped_handler);
+  app_message_register_outbox_failed(outbox_failed_handler);
+  app_message_register_outbox_sent(outbox_sent_handler);
+
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
   // Create main Window
   s_main_window = window_create();
+  window_set_click_config_provider(s_main_window, click_config_provider);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
-
-  // Subscribe to TickTimerService
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit(void) {
